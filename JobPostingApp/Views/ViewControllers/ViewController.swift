71a549e9a46c56004231de9c7424c70b9c5f9ac6
vipulRajput <thakurvipul07@gmail.com>
2020-01-26 14:11:48 +0530

    initial commit

---
+++ JobPostingApp/Views/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JobPostingApp
+//
+//  Created by Vipul Kumar on 26/01/20.
+//  Copyright © 2020 Vipul Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
